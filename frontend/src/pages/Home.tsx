150c7a7f676540716c4bb9b3f001e53fa1bea2fe
Ian Hart <ianhart7790@gmail.com>
2022-06-03 19:18:55 -0400

    feat: hide sign-in sign-up links for logged in user

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,25 +2,31 @@ import { Link } from 'react-router-dom';
 import '../styles/Home.scss';
 import '../styles/shared/Buttons.scss';
 import Footer from '../components/Mixed/Footer';
+import { useContext } from 'react';
+import { UserContext } from '../context/user';
+import { IUserContext } from '../interfaces';
 const Home = () => {
+  const { user } = useContext(UserContext) as IUserContext;
   return (
     <div className="home-container">
       <nav className="home-navigation">
         <header>
           <h1>Whiz</h1>
         </header>
-        <ul>
-          <li>
-            <Link to="/sign-in" className="link-button button-gradient">
-              Sign in
-            </Link>
-          </li>
-          <li>
-            <Link className="link-button" to="/sign-up">
-              Sign up
-            </Link>
-          </li>
-        </ul>
+        {!user.logged_in && (
+          <ul>
+            <li>
+              <Link to="/sign-in" className="link-button button-gradient">
+                Sign in
+              </Link>
+            </li>
+            <li>
+              <Link className="link-button" to="/sign-up">
+                Sign up
+              </Link>
+            </li>
+          </ul>
+        )}
       </nav>
       <div className="site-content">
         <p>Home</p>
